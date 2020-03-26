1591766b7e3cb91d0be531044a1d54ff7618bac7
David Daza <03dazal@gmail.com>
2020-03-26 12:18:22 -0500

    WIP

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        'contrail-one': ['"Contrail One"', 'cursive'],
+        'pt-sans-narrow': ['"PT Sans Narrow"', 'sans-serif']
+      },
+      colors: {
+        'mine-shaft': '#232323',
+        'electric-violet': '#5d3dfa',
+        'persian-blue': '#283edd',
+        rose: '#ff0059'
+      },
+      container: {
+        center: true,
+        padding: '1rem'
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
