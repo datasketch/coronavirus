103a5b2d78f6093edd249a9d9b893be507f036f1
David Daza <03dazal@gmail.com>
2020-03-27 11:52:09 -0500

    Filter viz and datasets pages with query param

---
+++ assets/js/datasets.js
@@ -0,0 +1,12 @@
+const eval = window.location.search.match(/scope=(\w+)\&?/);
+const scope = eval && eval.length ? eval[1] : '';
+
+if (scope) {
+  const cards = Array.from(document.querySelectorAll('.card.dataset'));
+
+  cards.forEach(function(card) {
+    if (card.dataset.scope !== scope) {
+      card.classList.add('hidden');
+    }
+  });
+}
